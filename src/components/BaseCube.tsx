a9729c0d380b265b4fea4b95d7affe1aef3526dc
Luismi <luismiac93@outlook.com>
2025-04-27 07:19:58 -0500

    [ft][LMAC-260425] Portfolio: new logo version size ajustment

---
+++ src/components/BaseCube.tsx
@@ -0,0 +1,51 @@
+import { cn } from "../lib/utils";
+
+interface BaseCubeProps {
+  className?: string;
+  variant: "large" | "small";
+}
+
+export const BaseCube = ({ className, variant }: BaseCubeProps) => {
+  const baseClass = variant === "large" ? "rubik-cube" : "small-rubik-cube";
+  
+  return (
+    <div className={cn(baseClass, "cube-base", className)}>
+      <div className="core"></div>
+      <div className="face front">
+        {[...Array(9)].map((_, i) => (
+          <div key={i} className="sticker sticker-front"></div>
+        ))}
+      </div>
+      <div className="face back">
+        {[...Array(9)].map((_, i) => (
+          <div key={i} className={`sticker sticker-back`}></div>
+        ))}
+      </div>
+      <div className="face left">
+        {[...Array(9)].map((_, i) => (
+          <div key={i} className={`sticker sticker-left`}></div>
+        ))}
+      </div>
+      <div className="face right">
+        {[...Array(9)].map((_, i) => (
+          <div
+            key={i}
+            className={`sticker sticker-right${
+              [1, 4, 7, 8].includes(i) ? " l-green" : ""
+            }`}
+          ></div>
+        ))}
+      </div>
+      <div className="face top">
+        {[...Array(9)].map((_, i) => (
+          <div key={i} className="sticker sticker-top"></div>
+        ))}
+      </div>
+      <div className="face bottom">
+        {[...Array(9)].map((_, i) => (
+          <div key={i} className="sticker sticker-bottom"></div>
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -1,42 +1,5 @@
+import { BaseCube } from "./BaseCube";
+
 export const Cube = () => {
-  return (
-    <div className="rubik-cube mr-5">
-      <div className="core"></div>
-      <div className="face front">
-        {[...Array(9)].map((_, i) => (
-          <div key={i} className="sticker sticker-front"></div>
-        ))}
-      </div>
-      <div className="face back">
-        {[...Array(9)].map((_, i) => (
-          <div key={i} className={`sticker sticker-back`}></div>
-        ))}
-      </div>
-      <div className="face left">
-        {[...Array(9)].map((_, i) => (
-          <div key={i} className={`sticker sticker-left`}></div>
-        ))}
-      </div>
-      <div className="face right">
-        {[...Array(9)].map((_, i) => (
-          <div
-            key={i}
-            className={`sticker sticker-right${
-              [1, 4, 7, 8].includes(i) ? " l-green" : ""
-            }`}
-          ></div>
-        ))}
-      </div>
-      <div className="face top">
-        {[...Array(9)].map((_, i) => (
-          <div key={i} className="sticker sticker-top"></div>
-        ))}
-      </div>
-      <div className="face bottom">
-        {[...Array(9)].map((_, i) => (
-          <div key={i} className="sticker sticker-bottom"></div>
-        ))}
-      </div>
-    </div>
-  );
+  return <BaseCube variant="large" className="mr-5" />;
 };
